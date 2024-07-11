1536b7c87d2e03d2fc18c97cb7654d38fd8ff25d
minseokiim <alstj991226@naver.com>
2024-07-12 03:41:33 +0900

    Rename: 구조 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
-import Header from "./components/Header";
+import Header from "@/components/Header";
 
 // 레이아웃(폰트, 골격)
 const openSans = Open_Sans({ subsets: ["latin"] });
