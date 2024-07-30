76b0aa234ea47b8592da612ae66f6572c7ad2cc6
minseokiim <alstj991226@naver.com>
2024-07-30 18:44:41 +0900

    Fix: 유저타입 에러 수정

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,12 +1,10 @@
+import { UserType } from "@/model/user";
 import NextAuth, { DefaultSession } from "next-auth";
 
 // * d.ts? next-auth에 기본적으로 정의된 타입 정의 파일
 
 declare module "next-auth" {
   interface Session {
-    user: {
-      username: string;
-    } & DefaultSession["user"];
-    // username은 기본으로 가지고 있지 않아서 만들어준 것
+    user: UserType;
   }
 }
