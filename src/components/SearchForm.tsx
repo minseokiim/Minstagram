69ab5b513305ae34e94fec5f4b73017f9102ef68
minseokiim <alstj991226@naver.com>
2024-08-23 17:31:56 +0900

    Feat: user filtering 추가 및 중복 코드 제거

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -5,14 +5,18 @@ import { useState } from "react";
 
 export default function SearchForm() {
   const { data, isLoading } = useSWR<UserType[]>("/api/user");
-  console.log(data);
-
   const [keyword, setKeyword] = useState<string>("");
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     console.log(e.target.value);
     setKeyword(e.target.value);
   };
+
+  const filteredData = data?.filter(
+    (user) =>
+      user.name.toLowerCase().includes(keyword.toLowerCase()) ||
+      user.username.toLowerCase().includes(keyword.toLowerCase())
+  );
 
   return (
     <div>
@@ -27,20 +31,17 @@ export default function SearchForm() {
         />
       </form>
 
-      {/* 1. 검색어 없을 때 전체 사용자 뜨게 */}
+      {/* 1. 검색어 없을 때 전체 사용자 뜨게
+       3. 검색어 있을 때에는 필터링 되어서 뜨게 */}
       <ul>
-        {data &&
-          !keyword &&
-          data.map((user, index) => (
-            <li key={index}>
-              {/* 5. 컴포넌트 만들어주기 */}
-              <span>{user.name} </span>
-              <span>{user.username}</span>
-            </li>
-          ))}
+        {filteredData?.map((user, index) => (
+          <li key={index}>
+            {/* 5. 컴포넌트 만들어서 빼주기 */}
+            <span>{user.name} </span>
+            <span>{user.username}</span>
+          </li>
+        ))}
       </ul>
-
-      {/* 3. 검색어 있을 때에는 필터링 되어서 뜨게 해주기 */}
 
       {/* 4. 고민 -> user.name으로 following 정보들을 받아오는 api 만들어 받아오기 */}
     </div>
