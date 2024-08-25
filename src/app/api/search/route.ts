035280e5973fa225ba0925c8770d5181e0c31191
minseokiim <alstj991226@naver.com>
2024-08-25 11:04:35 +0900

    Feat: force-dynamic 추가

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { searchUsers } from "@/service/user";
 
+export const dynamic = "force-dynamic";
+
 // 검색하는 키워드가 없는 경우 모든 user 리턴, 검색은 로그인 안해도 사용 가능
 export async function GET() {
   return searchUsers().then((data) => NextResponse.json(data));
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,6 +1,9 @@
 import UserSearch from "@/components/UserSearch";
 import { Metadata } from "next";
 
+//정적인 코드지만, ssg 되지 않게(요청 오면 수행하게) 추가
+export const dynamic = "force-dynamic";
+
 export const metadata: Metadata = {
   title: "Search User",
   description: "Search Users",
