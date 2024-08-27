69f17e2428b92ead08465a10d2f5d3ecf3171096
minseokiim <alstj991226@naver.com>
2024-08-27 21:32:26 +0900

    Refactor: 성능개선 위해 optimistic ui update & bound된 mutate사용

--- src/hooks/posts.ts
+++ src/hooks/posts.ts
@@ -1,20 +1,39 @@
 import { SimplePostType } from "@/model/post";
 import useSWR from "swr";
-import { useSWRConfig } from "swr";
+
+async function updateLike(id: string, like: boolean) {
+  return fetch("/api/likes", {
+    method: "PUT",
+    body: JSON.stringify({ id, like }),
+  }).then((res) => res.json());
+}
 
 export default function usePosts() {
   const {
     data: posts,
     isLoading,
     error,
+    mutate,
   } = useSWR<SimplePostType[]>("/api/posts");
-  const { mutate } = useSWRConfig();
 
   const setLike = (post: SimplePostType, username: string, like: boolean) => {
-    fetch("/api/likes", {
-      method: "PUT",
-      body: JSON.stringify({ id: post.id, like }),
-    }).then(() => mutate("api/posts")); //데이터가 변경되면 fetch
+    const newPost = {
+      ...post,
+      likes: like
+        ? [...post.likes, username]
+        : post.likes.filter((item) => item !== username),
+    };
+
+    const newPosts = posts?.map((post) =>
+      post.id === post.id ? newPost : post
+    );
+
+    return mutate(updateLike(post.id, like), {
+      optimisticData: newPosts,
+      populateCache: false,
+      revalidate: false,
+      rollbackOnError: true,
+    });
   };
 
   return { posts, isLoading, error, setLike };
