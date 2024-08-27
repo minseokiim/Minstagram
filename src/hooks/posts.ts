f98010415bd0cfc171b07e0d2a546d09ee06cee2
minseokiim <alstj991226@naver.com>
2024-08-27 21:38:53 +0900

    Docs: optimisticData 주석 추가

--- src/hooks/posts.ts
+++ src/hooks/posts.ts
@@ -13,7 +13,7 @@ export default function usePosts() {
     data: posts,
     isLoading,
     error,
-    mutate,
+    mutate, //global mutate가 아닌 api에 바운드 된 mutate
   } = useSWR<SimplePostType[]>("/api/posts");
 
   const setLike = (post: SimplePostType, username: string, like: boolean) => {
@@ -29,10 +29,10 @@ export default function usePosts() {
     );
 
     return mutate(updateLike(post.id, like), {
-      optimisticData: newPosts,
-      populateCache: false,
+      optimisticData: newPosts, //즉각적으로 ui 업데이트 위한 데이터 -> ui업데이트 후에 network요청
+      populateCache: false, //optimisticData 써줄거니까 false
       revalidate: false,
-      rollbackOnError: true,
+      rollbackOnError: true, //에러 발생시에 optimisticData 롤백
     });
   };
 
