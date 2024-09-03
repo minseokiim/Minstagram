d1d406c17bfee0e5d6445c20472e8bbbab5f5a0d
minseokiim <alstj991226@naver.com>
2024-09-03 19:43:59 +0900

    Feat: 게시글 작성 페이지 기본 세팅

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,4 +1,16 @@
+import { Metadata } from "next";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function page() {
-  return <div>new</div>;
+export const metadata: Metadata = {
+  title: "New Post",
+  description: "Create a new Post",
+};
+
+export default async function page() {
+  const session = await getServerSession();
+  if (!session) {
+    redirect("/auth/signin");
+  }
+  return <div>New Post</div>;
 }
