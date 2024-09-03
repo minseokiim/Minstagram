2d454067cdcc90a77ff5e8676301f7a0449e1fea
minseokiim <alstj991226@naver.com>
2024-09-03 20:05:42 +0900

    Feat: 사진 drag 기능 추가

--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -3,12 +3,47 @@ import { AuthUserType } from "@/model/user";
 import PostUserAvatar from "./PostUserAvatar";
 import FilesIcon from "./ui/icons/FilesIcon";
 import Button from "./Button";
+import { ChangeEvent, useState } from "react";
 
 type Props = {
   user: AuthUserType;
 };
 
 export default function NewPost({ user: { username, image } }: Props) {
+  const [dragging, setDragging] = useState(false);
+  const [file, setFile] = useState<File>();
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const files = e.target?.files;
+    if (files && files[0]) {
+      setFile(files[0]);
+      console.log(files[0]);
+    }
+  };
+
+  const handleDrag = (e: React.DragEvent) => {
+    if (e.type === "dragenter") {
+      setDragging(true);
+    } else if (e.type === "dragleave") {
+      setDragging(false);
+    }
+  };
+
+  const handleDragOver = (e: React.DragEvent) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e: React.DragEvent) => {
+    e.preventDefault();
+    setDragging(false);
+    const files = e.dataTransfer?.files;
+    if (files && files[0]) {
+      setFile(files[0]);
+      console.log(files[0]);
+    }
+  };
+
   return (
     <section>
       <PostUserAvatar username={username} image={image ?? ""} />
@@ -19,11 +54,19 @@ export default function NewPost({ user: { username, image } }: Props) {
           id="input-upload"
           type="file"
           accept="image/*"
+          onChange={handleChange}
         />
-        <label htmlFor="input-upload">
+        <label
+          htmlFor="input-upload"
+          onDragEnter={handleDrag}
+          onDragLeave={handleDrag}
+          onDragOver={handleDragOver}
+          onDrop={handleDrop}
+        >
           <FilesIcon />
           <p>Drag and Drop your image here or click</p>
         </label>
+
         <textarea
           name="text"
           id="input-text"
