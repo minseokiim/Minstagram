a22eb506f6b2525237dd57d22748194785102288
minseokiim <alstj991226@naver.com>
2024-09-04 18:29:37 +0900

    Fix: sanity 변수명 수정

--- src/service/sanity.ts
+++ src/service/sanity.ts
@@ -3,11 +3,11 @@ import imageUrlBuilder from "@sanity/image-url";
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 export const client = createClient({
-  projectId: process.env.SANITY_PROJECT_ID,
-  dataset: process.env.SANITY_DATASET,
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID,
+  dataset: process.env.SANITY_STUDIO_DATASET,
   useCdn: false, //동적 데이터니까 캐시 안함
   apiVersion: "2024-07-24", // 현재 날짜
-  token: process.env.SANITY_SECRET_TOKEN,
+  token: process.env.SANITY_STUDIO_SECRET_TOKEN,
   fetch: {
     cache: "no-store",
   },
@@ -22,4 +22,4 @@ export function urlFor(source: SanityImageSource) {
   return builder.image(source).width(800).url();
 }
 
-export const assetsURL = `https://${process.env.SANITY_PROJECT_ID}.api.sanity.io/v2021-03-25/assets/images/${process.env.SANITY_DATASET}`;
+export const assetsURL = `https://${process.env.SANITY_STUDIO_PROJECT_ID}.api.sanity.io/v2021-03-25/assets/images/${process.env.SANITY_DATASET}`;
